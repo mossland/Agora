60cad2eaa2b2e7d1bcc70ccba288b74897ab902e
Chohyuntae <chohyuntae815@gmail.com>
2024-09-25 14:53:33 +0900

    revert readme.md

--- src/utils/formatWallet.js
+++
@@ -1,9 +0,0 @@
-/**
- * Formats a wallet address to show only the first and last four characters.
- * @param {string} walletString - The full wallet address string.
- * @returns {string} - The formatted wallet address.
- */
-export function formatWallet(walletString) {
-    if (!walletString) return ""; // Handle potential null or undefined inputs
-    return `${walletString.slice(0, 4)}...${walletString.slice(-4)}`;
-  }
